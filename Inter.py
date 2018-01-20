e1c407600c38c51fcc3058511c6c48e50e72acfe
Professional Programmer <programingpro2018@gmail.com>
2018-01-20 14:12:40 +0200

    Aded 20.01.2018.14:12 data

--- Inter.py
+++ Inter.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python
-
 #Mini Interpreiaitor
 Text_string = 0
 import os
-#from tkinter import *
 import sys
-
+import pygame
 
 def main():
     while True:
         try:
             text = raw = input('Interpretaitor> ')
         except EOFError:
-	    print "Going out on KEYBOARD interrupt"
             break
         if not text:
             continue
@@ -21,11 +17,6 @@ def main():
         if text == '{write':
             Wi = input('              ?> ')
             print(Wi, ' }')
-        if text == 'while for h x[/':
-            whiled = input('                            ?>')
-            if whiled == 'var: nw v;':
-                var_nw = input('                         name>')
-                var_txt = input('                          var>')
         if text == 'var: file;in{':
             w_var = input('                          name>')
             if w_var == var_nw:
@@ -36,9 +27,18 @@ def main():
             var_txt = input('                          var>')
         if text == 'Qall':
             sys.exit()
-	    print "Leaving on <Qall> command"
+        if text == 'file: nw;':
+            name_f = input('              filename> ')
+            sys.stderr=open(name_f,'w')
+        if text == 'file: open;':
+            name_op = input('              filename> ')
+            f2 = open(name_op, 'r') 
+            fd1 = f2.read()
+            print(fd1)
+        if text == 'vers>c;<':
+            print(sys.platform)
 
-        
+
 
 if __name__ == '__main__':
     main()
